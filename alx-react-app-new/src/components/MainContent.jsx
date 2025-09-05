caf0a40cc183a358d3f6701474925c0aaadd832b
william kyalo <kyalowilliam969@gmail.com>
2025-09-05 20:36:25 +0300

    Add styled components in alx-react-app-new

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+ function MainContent() {
+  return (
+    <main style={{ padding: '20px', textAlign: 'center', backgroundColor: '#eef2f3' }}>
+      <p style={{ fontSize: '16px', color: 'darkgreen' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
