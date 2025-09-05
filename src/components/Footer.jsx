49509c8d89a3a497001a851c526066b1929c9b74
william kyalo <kyalowilliam969@gmail.com>
2025-09-05 23:05:02 +0300

    Add styled components project alx-react-app-new

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+ export default function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>© 2025 My Travel Site</p>
+    </footer>
+  );
+}
