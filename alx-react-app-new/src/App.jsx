7c3a7bec8cb4df0eaa02c714f5792688d681bd47
william kyalo <kyalowilliam969@gmail.com>
2025-09-05 21:50:25 +0300

    Add styled components in alx-react-app-new project

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,8 +1,7 @@
- import './App.css';
-import Header from './components/Header';
+ import Header from './components/Header';
 import MainContent from './components/MainContent';
-import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Footer from './components/Footer';
 
 function App() {
   return (
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,4 +1,4 @@
-function UserProfile(props) {
+ function UserProfile(props) {
   return (
     <div style={{ border: '1px solid gray', padding: '15px', margin: '15px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
       <h2 style={{ color: 'blue', marginBottom: '8px' }}>{props.name}</h2>
@@ -9,22 +9,3 @@ function UserProfile(props) {
 }
 
 export default UserProfile;
-
-import { useContext } from 'react';
-import UserContext from './UserContext';
-
-function UserDetails() {
-  const userData = useContext(UserContext);
-
-  return (
-    <div>
-      <p>Name: {userData.name}</p>
-      <p>Email: {userData.email}</p>
-    </div>
-  );
-}
-
-export default UserDetails;
-
-
-
