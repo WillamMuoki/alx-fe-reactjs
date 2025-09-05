a24ebdb84dbd187eb3481842ec01007dc454f4f0
william kyalo <kyalowilliam969@gmail.com>
2025-09-05 20:51:04 +0300

    Add styled components in alx-react-app-new

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,8 +1,9 @@
- import './App.css';
+import './App.css';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter'; // 👈 import Counter
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />  {/* 👈 Add Counter here */}
       <Footer />
     </>
   );
